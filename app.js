5e8366b15ee1c2fa76177fb3d9558c327eef3fe4
Your Name <nicoc12024@gmail.com>
2021-08-28 13:46:52 +0200

    w

---
+++ app.js
@@ -0,0 +1,15 @@
+const questionBox = document.querySelectorAll(".question-box");
+
+questionBox.forEach(function (singleB) {
+  const iconBox = singleB.querySelector(".icon-box");
+
+  iconBox.addEventListener("click", function () {
+    questionBox.forEach(function (item) {
+      if (item !== singleB) {
+        item.classList.remove("show-text");
+      }
+    });
+    singleB.classList.toggle("show-text");
+    console.log(iconBox);
+  });
+});
